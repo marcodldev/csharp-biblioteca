aad8fe6f4124b0a548d0a56452381e30c12b6165
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-03-21 14:50:32 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using csharp_biblioteca;
+
+UtenteRegistrato Utente1 = new UtenteRegistrato()
+{
+    Nome = "Sarah" ,
+    Cognome = "Verdi" ,
+    Email = "sarahverdi@test.it",
+    Password = "password" ,
+    RecapitoTelefonico = 3333333,
+};
+
+Console.WriteLine(Utente1);
---
+++ documenti.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class UtenteRegistrato
+    {
+
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public int RecapitoTelefonico { get; set; }
+
+        bool Registrato = true;
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Cognome: {Cognome}, Email: {Email}, Recapito telefonico: {RecapitoTelefonico}";
+        }
+
+    }
+
+    public class Documenti
+    {
+
+
+
+    }
+
+    /* Gli utenti registrati possono prendere in prestito dei documenti che sono di vario tipo (libri, DVD).
+    I documenti sono caratterizzati da:
+    - un codice identificativo di tipo stringa
+    - titolo
+    - anno
+    - settore (storia, matematica, economia, …)
+    - uno scaffale in cui è posizionato
+    - un autore (Nome, Cognome)
+    Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
+    L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, prendere in prestito registrando il periodo (Dal/Al) del prestito e il documento.
+    Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+    Creiamo anche una classe Biblioteca che contiene la lista dei documenti, la lista degli utenti e la lista dei prestiti.
+    Contiene inoltre i metodi per le ricerche e per l’aggiunta dei documenti, utenti e prestiti. */
+
+}
